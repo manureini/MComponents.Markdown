9d9e1802fa68700eaed68eb7ab954ac291f92f74
manureini <1554111+manureini@users.noreply.github.com>
2022-09-11 01:06:45 +0200

    a

--- MComponents.Markdown/MComponents.Markdown/Components/MarkdownMFieldGenerator.cs
+++ MComponents.Markdown/MComponents.Markdown/Components/MarkdownMFieldGenerator.cs
@@ -12,36 +12,45 @@ namespace MComponents.Markdown
     public class MarkdownMFieldGenerator : MFieldGenerator
     {
         [Parameter]
-        public string Text { get; set; }      
-        
+        public string Text { get; set; }
+
         [Parameter]
         public string Label { get; set; }
 
         protected MarkupString mHtml;
 
+        protected string mOldText;
+
         public MarkdownMFieldGenerator()
         {
+            mHtml = MarkdownHelper.RenderMarkdown(Text);
+
             Template = (context) => (builder) =>
             {
-                if(Label != null)
+                if (Label != null)
                 {
                     builder.OpenElement(30, "label");
-                    builder.AddAttribute(31, "class", "col-sm-12 col-form-label"); 
+                    builder.AddAttribute(31, "class", "col-sm-12 col-form-label");
                     builder.AddContent(32, Label);
                     builder.CloseElement();
                 }
 
                 builder.OpenElement(50, "div");
                 builder.AddAttribute(51, "class", "m-markdown-container");
-                builder.AddContent(52, MarkdownHelper.RenderMarkdown(Text));
+                builder.AddContent(52, mHtml);
                 builder.CloseElement();
             };
         }
 
         protected override void OnParametersSet()
         {
-          //  mHtml = MarkdownHelper.RenderMarkdown(Text);
-          //not working in accre forms
+            if (mOldText != Text)
+            {
+                mOldText = Text;
+                //not working in accre forms
+                mHtml = MarkdownHelper.RenderMarkdown(Text);
+                Form.InvokeStateHasChanged();
+            }
         }
     }
 }
